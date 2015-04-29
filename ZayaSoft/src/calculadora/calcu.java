55d621898b1bf7b126f94bee9f9073dbfc7c6283
deividko <david94.correa@gmail.com>
2015-04-29 09:43:27 +0200

    v1.6

---
+++ ZayaSoft/src/calculadora/calcu.java
@@ -0,0 +1,22 @@
+package calculadora;
+
+public class calcu {
+    public static void main(String[] args) {
+
+        int numero = 4;
+      
+        switch (numero) {
+            case 1:   System.out.println("sumar");
+                     break;
+            case 2:  System.out.println("restar"); 
+                     break;
+            case 3:  System.out.println("Multiplicar");
+                     break;
+            case 4:  System.out.println( "dividir");
+                     break;
+           
+            
+        }
+        
+    }
+}
