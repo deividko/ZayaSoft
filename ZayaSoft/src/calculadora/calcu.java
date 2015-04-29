1d3cb3c3bf6692ff001b3c94df5e22c6bd395014
deividko <david94.correa@gmail.com>
2015-04-29 09:52:09 +0200

    v1.7

--- ZayaSoft/src/calculadora/calcu.java
+++ ZayaSoft/src/calculadora/calcu.java
@@ -1,10 +1,15 @@
 package calculadora;
 
+import java.util.Scanner;
+
 public class calcu {
     public static void main(String[] args) {
 
-        int numero = 4;
-      
+        int numero;
+        Scanner teclado=new Scanner (System.in);
+        System.out.println("dame una opcion y cero para salir");
+        numero=teclado.nextInt();
+      while (numero!=0){
         switch (numero) {
             case 1:   System.out.println("sumar");
                      break;
@@ -14,9 +19,13 @@ public class calcu {
                      break;
             case 4:  System.out.println( "dividir");
                      break;
-           
-            
+                        
         }
-        
+        System.out.println("dame una opcion y cero para salir");
+        numero=teclado.nextInt();
+      }
+      if(numero==0){
+    	  System.out.println("has salido de la calculadora");
+      }
     }
 }
